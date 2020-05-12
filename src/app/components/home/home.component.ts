494a0c7b0d02da9e034f8506e8d3633dec97326e
IvanSaRo <ivansancrodr@gmail.com>
2020-05-13 00:22:45 +0200

    Incluiso header, 1ª conexión API

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
   selector: "app-home",
@@ -6,7 +7,9 @@ import { Component, OnInit } from "@angular/core";
   styles: [],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private spotifyService: SpotifyService) {
+    this.spotifyService.getNewReleases();
+  }
 
   ngOnInit() {}
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class SpotifyService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getNewReleases() {
+    let headers = new HttpHeaders();
+    headers = headers.append(
+      "Authorization",
+      "Bearer BQBX3RSSQA8Ln-QlmM44TZ2xL3Lm1a7ZljT2Wgni90bqHnmebfpI6-rfWr1l6JuJaOjw1mUfhqrAB0Ixu0Q"
+    );
+    this.httpClient
+      .get("https://api.spotify.com/v1/browse/new-releases", { headers })
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }

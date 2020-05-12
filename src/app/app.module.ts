085a509893745221c709d7ddd1248f9b3c031d1a
IvanSaRo <ivansancrodr@gmail.com>
2020-05-12 23:40:01 +0200

    Creado servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
@@ -10,8 +12,10 @@ import { ArtistaComponent } from "./components/artista/artista.component";
 import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 
 //RUTAS
-
 import { ROUTES } from "./app.routes";
+
+//SERVICIOS
+import { SpotifyService } from "./services/spotify.service";
 
 @NgModule({
   declarations: [
@@ -21,8 +25,13 @@ import { ROUTES } from "./app.routes";
     ArtistaComponent,
     NavbarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(ROUTES)],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES),
+  ],
+  providers: [SpotifyService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

3c6b1e7c79f3e270d51b24a83ef616f3621fae37
IvanSaRo <ivansancrodr@gmail.com>
2020-05-13 23:58:53 +0200

    Search funciona pero da errores con los artistas que no tienen img[0]

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { SpotifyService } from "src/app/services/spotify.service";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styles: []
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styles: [],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  artistas: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private spotifyService: SpotifyService) {}
+  buscar(termino: string) {
+    this.spotifyService.getArtista(termino).subscribe((data: any) => {
+      this.artistas = data.artists.items;
+      console.log(this.artistas);
+    });
   }
-
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,10 +10,24 @@ export class SpotifyService {
     let headers = new HttpHeaders();
     headers = headers.append(
       "Authorization",
-      "Bearer BQBFhI1pUMDa6Sf_PIpm6ylUZWg_gsAKFNtoG2_rGtb4RMIpuapcXaXGH03ruMfwT1l5t04Ku6FFz3vnrTE"
+      "Bearer BQCYcfrLG8C1uzFasRyhQJJPYTkKD8u-24yZvaw6Byv4tCgp-vdO9b28ozcT64WBYa2MFifa29WEFhRi1pg"
     );
     return this.httpClient.get(
       "https://api.spotify.com/v1/browse/new-releases?limit=20",
+      {
+        headers,
+      }
+    );
+  }
+  //    https://api.spotify.com/v1/search?q=madonna&type=artist&limit=20
+  getArtista(termino: string) {
+    let headers = new HttpHeaders();
+    headers = headers.append(
+      "Authorization",
+      "Bearer BQCYcfrLG8C1uzFasRyhQJJPYTkKD8u-24yZvaw6Byv4tCgp-vdO9b28ozcT64WBYa2MFifa29WEFhRi1pg"
+    );
+    return this.httpClient.get(
+      `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
       {
         headers,
       }
